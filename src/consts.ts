207b82d3ea582963cb619bf743cbbd27945ac062
Rua Haszard <haszari@cartoonbeats.com>
2025-08-31 11:33:52 +1200

    updated site description/subtitle

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Cartoon Beats Reality';
-export const SITE_DESCRIPTION = 'My outlet for electronic abstract music and art';
+export const SITE_DESCRIPTION = 'Abstract music, art, photograpy & culture from Tāmaki Makaurau';
